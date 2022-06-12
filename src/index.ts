969cd6b14ec74ed237fa9d6a2db7ad080cf89109
Sylvain Rivierre <sylvainrivierre@gmail.com>
2022-06-12 19:20:20 +0200

    Trying interactions

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { SlashCommandBuilder } from "@discordjs/builders"
 import { REST } from "@discordjs/rest"
-import { Routes } from "discord-api-types/v10"
-import { Application, Client } from "discord.js"
-import { clientId, guildId, Token } from "../config.json"
+import { APIMessage, Routes } from "discord-api-types/v10"
+import { Application, CacheType, Client, CommandInteraction, Interaction, Message, MessageEmbed, MessageReaction, TextChannel, User } from "discord.js"
+import { clientId, guildId, token } from "../config.json"
 
 
 const client = new Client({ intents: 8 })
-const rest = new REST({ version: "10" }).setToken(Token)
+const rest = new REST({ version: "10" }).setToken(token)
 
 
 const cleanCommands = async () => {
@@ -17,18 +17,45 @@ const cleanCommands = async () => {
 
 }
 
+const filterReactions = (mr: MessageReaction, user: User): boolean | Promise<boolean> => {
+    return [":cross:"].indexOf(mr.emoji.identifier) >= 0
+}
 
+// Reply with a proposition
+const command = async (interaction: Interaction<CacheType>) => {
+    if (interaction instanceof CommandInteraction) {
+
+
+        const embed = new MessageEmbed()
+            .setTitle("What do you want ?")
+            .setDescription("Eat, Yell, Drink ?")
+
+        const replied = await interaction.reply({ embeds: [embed], fetchReply: true }) as any
+        const channel = await interaction.guild.channels.fetch(replied["channel_id"] || replied["channelId"]) as TextChannel
+
+        const repliedObj = await channel.messages.fetch(replied.id)
+
+        console.log(repliedObj.author.username)
+        const collector = repliedObj.createReactionCollector()
+        collector.on("collect", r => console.log(`Collected ${r.emoji.name}`))
+        collector.on("end", collected => console.log(`Collected ${collected.size} items`))
+    }
+}
 
 const run = async () => {
 
-    await cleanCommands()
 
     const cmd = new SlashCommandBuilder()
-        .setName("toast")
+        .setName("m")
         .setDescription("plop")
     await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: [cmd] })
+
+    client.on("interactionCreate", command)
+    client.on("messageReactionAdd", console.log)
+
 }
 
-client.login(Token).then(run).catch(({ message, stack }) => {
+
+client.login(token).then(run).catch(({ message, stack }) => {
     console.log(`${message} : ${stack}`)
 })
